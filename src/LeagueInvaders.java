433eb45e3129b3b5ad059b0208d956d8a4d33322
Ananya1707 <55816283+Ananya1707@users.noreply.github.com>
2021-05-10 16:57:54 -0700

    Finished Runner Class

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,24 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	public static void main(String[] args) {
+		LeagueInvaders game  = new LeagueInvaders();
+		game.setup();
+	}
+	
+	LeagueInvaders(){
+		frame = new JFrame();
+		
+	}
+	
+	void setup() {
+		frame.setSize(WIDTH,HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
 
 }
